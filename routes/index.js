33b794429b32556679e6f17a9ba2a5f55ac9894a
Peter Kona <ikona23@gmail.com>
2016-10-03 12:25:15 +1300

    test

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var getCams = require('../public/javascripts/data.json')
+var _ = require('lodash')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
